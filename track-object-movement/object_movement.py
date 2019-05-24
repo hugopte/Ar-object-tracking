3ec7fdb83e26ccb6ea00e85cb517d861f8318650
hugopte <hugo.pette@viacesi.fr>
2019-05-24 15:22:15 +0200

    Add files via upload

--- track-object-movement/object_movement.py
+++ track-object-movement/object_movement.py
@@ -15,9 +15,9 @@ import time
 # construct the argument parse and parse the arguments
 ap = argparse.ArgumentParser()
 ap.add_argument("-v", "--video",
-	help="path to the (optional) video file")
+                help="path to the (optional) video file")
 ap.add_argument("-b", "--buffer", type=int, default=32,
-	help="max buffer size")
+                help="max buffer size")
 args = vars(ap.parse_args())
 
 # define the lower and upper boundaries of the "green"
@@ -35,131 +35,140 @@ direction = ""
 # if a video path was not supplied, grab the reference
 # to the webcam
 if not args.get("video", False):
-	vs = VideoStream(src=0).start()
+    vs = VideoStream(src=0).start()
 
 # otherwise, grab a reference to the video file
 else:
-	vs = cv2.VideoCapture(args["video"])
+    vs = cv2.VideoCapture(args["video"])
 
 # allow the camera or video file to warm up
 time.sleep(2.0)
 
 # keep looping
 while True:
-	# grab the current frame
-	frame = vs.read()
+    # grab the current frame
+    frame = vs.read()
 
-	# handle the frame from VideoCapture or VideoStream
-	frame = frame[1] if args.get("video", False) else frame
+    # handle the frame from VideoCapture or VideoStream
+    frame = frame[1] if args.get("video", False) else frame
 
-	# if we are viewing a video and we did not grab a frame,
-	# then we have reached the end of the video
-	if frame is None:
-		break
+    # if we are viewing a video and we did not grab a frame,
+    # then we have reached the end of the video
+    if frame is None:
+        break
 
-	# resize the frame, blur it, and convert it to the HSV
-	# color space
-	frame = imutils.resize(frame, width=600)
-	blurred = cv2.GaussianBlur(frame, (11, 11), 0)
-	hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
+    # resize the frame, blur it, and convert it to the HSV
+    # color space
+    frame = imutils.resize(frame, width=600)
+    blurred = cv2.GaussianBlur(frame, (11, 11), 0)
+    hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
 
-	# construct a mask for the color "green", then perform
-	# a series of dilations and erosions to remove any small
-	# blobs left in the mask
-	mask = cv2.inRange(hsv, greenLower, greenUpper)
-	mask = cv2.erode(mask, None, iterations=2)
-	mask = cv2.dilate(mask, None, iterations=2)
+    # construct a mask for the color "green", then perform
+    # a series of dilations and erosions to remove any small
+    # blobs left in the mask
+    mask = cv2.inRange(hsv, greenLower, greenUpper)
+    mask = cv2.erode(mask, None, iterations=2)
+    mask = cv2.dilate(mask, None, iterations=2)
 
-	# find contours in the mask and initialize the current
-	# (x, y) center of the ball
-	cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
-		cv2.CHAIN_APPROX_SIMPLE)
-	cnts = imutils.grab_contours(cnts)
-	center = None
 
-	# only proceed if at least one contour was found
-	if len(cnts) > 0:
-		# find the largest contour in the mask, then use
-		# it to compute the minimum enclosing circle and
-		# centroid
-		c = max(cnts, key=cv2.contourArea)
-		((x, y), radius) = cv2.minEnclosingCircle(c)
-		M = cv2.moments(c)
-		center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
 
-		# only proceed if the radius meets a minimum size
-		if radius > 10:
-			# draw the circle and centroid on the frame,
-			# then update the list of tracked points
-			cv2.circle(frame, (int(x), int(y)), int(radius),
-				(0, 255, 255), 2)
-			cv2.circle(frame, center, 5, (0, 0, 255), -1)
-			pts.appendleft(center)
+    cv2.imshow("Frame2", mask)
+    # find contours in the mask and initialize the current
+    # find contours in the mask and initialize the current
+    # (x, y) center of the ball
+    cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    cnts = imutils.grab_contours(cnts)
+    center = None
+    frame[:] = (0,0,0)
 
-	# loop over the set of tracked points
-	for i in np.arange(1, len(pts)):
-		# if either of the tracked points are None, ignore
-		# them
-		if pts[i - 1] is None or pts[i] is None:
-			continue
+    try:
+        # only proceed if at least one contour was found
+        if len(cnts) > 0:
+            # find the largest contour in the mask, then use
+            # it to compute the minimum enclosing circle and
+            # centroid
+            c = max(cnts, key=cv2.contourArea)
+            ((x, y), radius) = cv2.minEnclosingCircle(c)
+            M = cv2.moments(c)
+            center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
 
-		# check to see if enough points have been accumulated in
-		# the buffer
-		if counter >= 10 and i == 1 and pts[-10] is not None:
-			# compute the difference between the x and y
-			# coordinates and re-initialize the direction
-			# text variables
-			dX = pts[-10][0] - pts[i][0]
-			dY = pts[-10][1] - pts[i][1]
-			(dirX, dirY) = ("", "")
+            # only proceed if the radius meets a minimum size
+            if radius > 10:
+                # draw the circle and centroid on the frame,
+                # then update the list of tracked points
+                cv2.circle(frame, (int(x), int(y)), int(radius),
+                           (0, 255, 255), -1)
 
-			# ensure there is significant movement in the
-			# x-direction
-			if np.abs(dX) > 20:
-				dirX = "East" if np.sign(dX) == 1 else "West"
+                pts.appendleft(center)
 
-			# ensure there is significant movement in the
-			# y-direction
-			if np.abs(dY) > 20:
-				dirY = "North" if np.sign(dY) == 1 else "South"
 
-			# handle when both directions are non-empty
-			if dirX != "" and dirY != "":
-				direction = "{}-{}".format(dirY, dirX)
 
-			# otherwise, only one direction is non-empty
-			else:
-				direction = dirX if dirX != "" else dirY
+        # loop over the set of tracked points
+        for i in np.arange(1, len(pts)):
+            # if either of the tracked points are None, ignore
+            # them
+            if pts[i - 1] is None or pts[i] is None:
+                continue
 
-		# otherwise, compute the thickness of the line and
-		# draw the connecting lines
-		thickness = int(np.sqrt(args["buffer"] / float(i + 1)) * 2.5)
-		cv2.line(frame, pts[i - 1], pts[i], (0, 0, 255), thickness)
+            # check to see if enough points have been accumulated in
+            # the buffer
+            if counter >= 10 and i == 1 and pts[-10] is not None:
+                # compute the difference between the x and y
+                # coordinates and re-initialize the direction
+                # text variables
+                dX = pts[-10][0] - pts[i][0]
+                dY = pts[-10][1] - pts[i][1]
+                (dirX, dirY) = ("", "")
 
-	# show the movement deltas and the direction of movement on
-	# the frame
-	cv2.putText(frame, direction, (10, 30), cv2.FONT_HERSHEY_SIMPLEX,
-		0.65, (0, 0, 255), 3)
-	cv2.putText(frame, "dx: {}, dy: {}".format(dX, dY),
-		(10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX,
-		0.35, (0, 0, 255), 1)
+                # ensure there is significant movement in the
+                # x-direction
+                if np.abs(dX) > 20:
+                    dirX = "East" if np.sign(dX) == 1 else "West"
 
-	# show the frame to our screen and increment the frame counter
-	cv2.imshow("Frame", frame)
-	key = cv2.waitKey(1) & 0xFF
-	counter += 1
+                # ensure there is significant movement in the
+                # y-direction
+                if np.abs(dY) > 20:
+                    dirY = "North" if np.sign(dY) == 1 else "South"
 
-	# if the 'q' key is pressed, stop the loop
-	if key == ord("q"):
-		break
+                # handle when both directions are non-empty
+                if dirX != "" and dirY != "":
+                    direction = "{}-{}".format(dirY, dirX)
+
+                # otherwise, only one direction is non-empty
+                else:
+                    direction = dirX if dirX != "" else dirY
+
+            # otherwise, compute the thickness of the line and
+            # draw the connecting lines
+            thickness = int(np.sqrt(args["buffer"] / float(i + 1)) * 2.5)
+            #cv2.line(frame, pts[i - 1], pts[i], (0, 0, 255), thickness)
+    except:
+        print("Erreur")
+    # show the movement deltas and the direction of movement on
+    # the frame
+    cv2.putText(frame, direction, (10, 30), cv2.FONT_HERSHEY_SIMPLEX,
+                0.65, (0, 0, 255), 3)
+    cv2.putText(frame, "dx: {}, dy: {}".format(dX, dY),
+                (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX,
+                0.35, (0, 0, 255), 1)
+
+    # show the frame to our screen and increment the frame counter
+    cv2.imshow("Frame", frame)
+
+    key = cv2.waitKey(1) & 0xFF
+    counter += 1
+
+    # if the 'q' key is pressed, stop the loop
+    if key == ord("q"):
+        break
 
 # if we are not using a video file, stop the camera video stream
 if not args.get("video", False):
-	vs.stop()
+    vs.stop()
 
 # otherwise, release the camera
 else:
-	vs.release()
+    vs.release()
 
 # close all windows
 cv2.destroyAllWindows()
